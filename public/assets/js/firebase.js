eb41d9884b9f1a3134b7c9e45fc07c00d2b3ba31
ilmong06 <ilmongcoding@gmail.com>
2024-10-04 23:34:13 +0900

    로그아웃 추가

--- public/assets/js/firebase.js
+++ public/assets/js/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAfiRIkNZesOTlkfQTWizBqdDkjmWJQvRo",
-  authDomain: "ilmongblog.firebaseapp.com",
-  projectId: "ilmongblog",
-  storageBucket: "ilmongblog.appspot.com",
-  messagingSenderId: "25466040786",
-  appId: "1:25466040786:web:fc77d47eedf7c73280d80b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 // Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
